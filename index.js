5bb7b272bf795155abc8380ade4160959956a6fb
10543218 <53287639+10543218@users.noreply.github.com>
2020-03-15 21:25:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 // 顯示左側選單
 let menuIsOpen = false;
+let burgerIsClick = false;
 let ShowMenu = () => {
 	const menu = document.getElementById("navBelow");
 	menu.style.visibility = menuIsOpen ? "hidden" : "visible";
 	menuIsOpen = !menuIsOpen;
+	
+	const burger = document.getElementById("burger");
+	burger.style.backgroundColor = burgerIsClick ? "#dddddd" : "#cccccc";
+	burger.style.borderRadius = "999em";
+	burgerIsClick = !burgerIsClick;
 }
